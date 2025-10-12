35b35696a348039891963430b5e2d802140ee7c0
Ghazi Alchammat <alchammatg@gmail.com>
2025-10-12 23:58:23 +0400

    chore: sets up streamer models and page

--- internal/models/models.go
+++ internal/models/models.go
@@ -1,1 +1,22 @@
 package models
+
+type Streamer struct {
+	Name    string   `json:"name" db:"name"`
+	Streams []Stream `json:"streams,omitempty" db:"-"`
+}
+
+type StreamID int64
+
+type Stream struct {
+	ID       StreamID
+	Platform string
+	Username string
+	Streamer string
+}
+
+type Broadcast struct {
+	ID        int64
+	StreamID  StreamID
+	StartTime int64
+	EndTime   int64
+}
