fd39e48ae8291d325de957f0bd37ed22c2306fab
Ghazi Alchammat <alchammatg@gmail.com>
2026-01-23 22:40:42 -0700

    wip: removes dummy data and leverage useLolMatches hook with improvements

--- frontend/src/hooks/useLolMatches.ts
+++ frontend/src/hooks/useLolMatches.ts
@@ -2,27 +2,25 @@ import { useState } from 'react';
 import { models } from '@wails/go/models';
 import { ListMatches } from '@wails/go/riot/RiotClient';
 
+type fetchMatchesProps = {
+  filter: models.LolMatchFilter,
+  limit: number,
+  offset: number
+}
+
 export interface UseLolMatchesReturn {
   matches: models.LeagueOfLegendsMatch[];
   loading: boolean;
   error: string | null;
-  fetchMatches: (
-    filter: models.LolMatchFilter,
-    limit: number,
-    offset: number
-  ) => Promise<void>;
+  fetchMatches: (props: fetchMatchesProps) => Promise<void>;
 }
 
-export function useLolMatches(account: models.LeagueOfLegendsAccount): UseLolMatchesReturn {
+export default function useLolMatches(account: models.LeagueOfLegendsAccount): UseLolMatchesReturn {
   const [matches, setMatches] = useState<models.LeagueOfLegendsMatch[]>([]);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState<string | null>(null);
 
-  const fetchMatches = async (
-    filter: models.LolMatchFilter,
-    limit: number,
-    offset: number
-  ) => {
+  const fetchMatches = async (props: fetchMatchesProps) => {
     try {
       setLoading(true);
       const result = await ListMatches(filter, account, limit, offset);
